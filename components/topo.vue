c3e28b92e46a456450dca7ab60f1114affef2674
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-09-28 18:06:10 -0300

    create logo

--- components/topo.vue
+++ components/topo.vue
@@ -3,9 +3,9 @@
     <header>
       <div class="header container justify-content-center align-items-center">
         <div class="box-title">
-          <h1>
-            DevCards
-          </h1>
+          <div class="logo">
+            <img src="~/assets/images/logo.svg" alt="" width="200">
+          </div>
           <p>O jeito mais facíl de você se tornar um Dev.</p>
         </div>
       </div>
@@ -31,12 +31,15 @@ export default {
 
 <style lang="scss" scoped>
 .header {
-  min-height: 50vh;
+  min-height: 60vh;
   background: linear-gradient(10deg, #333, #222);
   color: #fff;
     .box-title {
       transform: translateY(-50px);
       text-align: center;
+        .logo {
+          padding-bottom: 20px
+        }
     }
 }
 
