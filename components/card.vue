a36eda1e1985188b2b1099493c8fa73e62da86e6
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-09-28 15:26:31 -0300

    cards

---
+++ components/card.vue
@@ -0,0 +1,121 @@
+<template>
+
+    <div class="">
+      <div class="card css container column"  v-if="type === 'CSS'">
+        <div class="number container justify-content-between">
+          <slot name="index"></slot>
+        </div>
+        <div class="language">
+          <h4>
+            <slot name="language"></slot>
+          </h4>
+        </div>
+        <div class="text">
+          <h2>
+            <slot name="text"></slot>
+          </h2>
+        </div>
+        <div class="tags">
+          <slot name="tag">
+          </slot>
+        </div>
+      </div>
+
+
+
+      <div class="card js container column" v-if="type === 'JS'">
+        <div class="number container justify-content-between">
+          <slot name="index"></slot>
+        </div>
+        <div class="language">
+          <h4>
+            <slot name="language"></slot>
+          </h4>
+        </div>
+        <div class="text">
+          <h2>
+            <slot name="text"></slot>
+          </h2>
+        </div>
+        <div class="tags">
+          <slot name="tag">
+          </slot>
+        </div>
+      </div>
+
+
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'cards',
+  data() {
+    return {
+      isCss: true,
+      isJs: true
+    }
+  },
+  props: {
+  		type: {
+  			type: String
+  		}
+  	},
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.number {
+    // background: red
+}
+.language {
+    // background: yellow
+}
+.text {
+    // background: orange;
+    flex-grow: 1;
+}
+.tags {
+    // background: blue
+}
+
+.card {
+    margin: 0 auto;
+    width: 90%;
+    padding: 20px;
+    box-sizing: border-box;
+    margin-bottom: 10px;
+    min-height: 350px;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: .3s;
+    box-shadow: 0 0 20px 0 rgba(0,0,0,.2);
+      &:hover {
+        transform: scale(1.1,1.1);
+        box-shadow: 0 10px 20px 0 rgba(0,0,0,.2);
+      }
+}
+.css {
+    color: #fff;
+    background: #fff;
+    background: linear-gradient(10deg, #6096FB, #64D4EA);
+}
+.js {
+    color: #fff;
+    background: #fff;
+    background: linear-gradient(10deg, #F8A856, #F85555);
+}
+.tag li {
+    margin-right: 5px;
+    background: #fff;
+    color: #414141;
+    padding: 5px 10px;
+    border-radius: 20px;
+    font-weight: 900px;
+    font-size: 12px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,98 @@
 <template>
-  <div>
-  <typography></typography>
+<div class="bg">
+  <!-- <typography></typography> -->
+
+
+  <div class="container-cards container wrap size margin">
+    <div class="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-12 margin-bottom-30" v-for="(x, index) in cards">
+      <div class="">
+
+        <cards type="CSS" v-if="x.language === 'CSS'">
+          <span slot="index">#00{{index + 1}}</span>
+          <span slot="language">{{x.language}}</span>
+          <span slot="text">{{x.text}}</span>
+          <span slot="tag"><div >
+            <ul class="tag list-none container wrap">
+              <li v-for="tag in x.tags"><b>{{tag}}</b></li>
+            </ul>
+          </div></span>
+        </cards>
+
+        <cards type="JS" v-if="x.language === 'JS'">
+          <span slot="index">#00{{index + 1}}</span>
+          <span slot="language">{{x.language}}</span>
+          <span slot="text">{{x.text}}</span>
+          <span slot="tag"><div >
+            <ul class="tag list-none container wrap">
+              <li v-for="tag in x.tags"><b>{{tag}}</b></li>
+            </ul>
+          </div></span>
+        </cards>
+
+      </div>
+    </div>
   </div>
+
+
+
+
+</div>
 </template>
 
 <script>
-  import typography from '@/components/typography';
+import typography from '@/components/typography';
+import cards from '@/components/card';
 
-  export default {
-    components: {
-      typography
-    },
-    data() {
-      return {
-      }
+export default {
+  components: {
+    typography,
+    cards
+  },
+  data() {
+    return {
+      cards: [{
+          language: 'CSS',
+          text: 'Use flebox',
+          tags: ['CSS', 'Flebox', 'Grid']
+        }, {
+          language: 'CSS',
+          text: 'Não use float',
+          tags: ['CSS', 'Flebox', 'Grid']
+        },
+        {
+          language: 'CSS',
+          text: 'Não use !important',
+          tags: ['CSS', 'Flebox', 'Grid']
+        },
+        {
+          language: 'JS',
+          text: 'Não use var',
+          tags: ['JS', 'Variables']
+        },
+        {
+          language: 'JS',
+          text: 'Use Const',
+          tags: ['JS', 'Variables']
+        },
+        {
+          language: 'JS',
+          text: 'Use Let',
+          tags: ['JS', 'Variables']
+        }
+      ]
     }
   }
+}
 </script>
 
 <style lang="scss">
-  @import './assets/css/main.scss';
+@import './assets/css/main.scss';
+.bg {
+    // background: #f2f2f2;
+    min-height: 100vh;
+}
+.container-cards {
+    // border: 1px solid #ddd;
+    padding-top: 10px;
+}
 </style>
